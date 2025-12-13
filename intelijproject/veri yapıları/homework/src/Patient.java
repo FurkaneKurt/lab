06072b989f981bb1ee0bc4af139e6ec90d0a84d1
furkan enes kurt <furkaneneskurt25@gmail.com>
2025-12-13 23:30:52 +0300

    init commit

--- intelijproject/veri yapıları/homework/src/Patient.java
+++ intelijproject/veri yapıları/homework/src/Patient.java
@@ -6,7 +6,7 @@ public class Patient {
     int age;
     Patient next;
 
-    public Patient(int id,String name, int severity, int age ){
+    public Patient(int id,String name, int severity, int age ){ //constructor for patient
         this.id=id;
         this.name=name;
         this.severity=severity;
